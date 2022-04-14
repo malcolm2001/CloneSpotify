d5950f4d962114e8b178ca74043e44bf9d658925
Malcolm Rodrigues <70242306+malcolm2001@users.noreply.github.com>
2022-04-14 19:10:29 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,73 @@
+var audioPlayer = document.getElementById('audioplayer');
+var loaded = false;
+var playBtn = document.getElementById('playBtn');
+var pauseBtn = document.getElementById('pauseBtn');
+
+pauseBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    
+    playBtn.style.display = "inline" ;
+    pauseBtn.style.display = "none" ;
+    audioPlayer.pause();
+
+
+    return false;
+});
+
+playBtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    
+    playBtn.style.display = "none" ;
+    pauseBtn.style.display = "inline" ;
+    audioPlayer.play();
+
+
+    return false;
+});
+
+const playsong = (file) => {
+    if(loaded == false){
+        audioPlayer.innerHTML = `<source src="`+file+`" type="audio/mp3" />`;
+        loaded = true
+    }
+
+
+    audioPlayer.play();
+
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
+
+
+}
+
+
+document.querySelectorAll('.main__col2').forEach(item =>{
+    item.addEventListener('click', event=>{
+        let image = item.getAttribute('data-image');
+        let artist = item.getAttribute('data-artist');
+        let song = item.getAttribute('data-song');
+        let file = item.getAttribute('data-file');
+
+
+        let playerArtistcomponent = document.getElementsByClassName('player__artist');
+
+        playerArtistcomponent[0].innerHTML = `
+            <img src="`+image+`" />
+            <h3>`+artist+`<br/><span>`+song+`</span></h3>
+        `;
+
+        playsong(file);
+
+
+
+    })
+    
+});
+
+document.querySelectorAll('.main__col').forEach(item =>{
+    item.addEventListener('click', event=>{
+        alert('Infelizmente não implementei isso ainda.');
+        
+    })
+    
+});
